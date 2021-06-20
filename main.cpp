b7cc65759f39bed4510786f01e3f1a0abc4818e9
pyskonus <pyskob190201@gmail.com>
2021-06-20 18:40:40 +0300

    code conventions

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@
 #include <Eigen/SparseCore>
 #include <Eigen/Core>
 #include <Eigen/SparseLU>
-#include <Eigen/IterativeLinearSolvers>
+/*#include <Eigen/IterativeLinearSolvers>*/
 #include <vector>
 #include <map>
-#include <fstream>
+/*#include <fstream>*/
 #include <thread>
 #include "inc/rw_image.h"
 #include "inc/main_computations.h"
 
 void main_alg(Eigen::VectorXd& X, const std::vector<std::pair<unsigned, unsigned>>& order,
-              const std::map<std::pair<unsigned, unsigned>, unsigned>& seeds, const PNG& wrapper) {
+              const std::map<std::pair<unsigned, unsigned>, unsigned>& seeds, const PNG& wrapper)
+{
     /// L_u * x = b. Find x
     Eigen::/*ConjugateGradient*/SparseLU<Eigen::SparseMatrix<double>> solver;   /// TODO: solver type
     Eigen::VectorXd b{order.size() - seeds.size()};
     b.setZero();
-    for (unsigned i = 0; i < b.size(); ++i) {
-        b.coeffRef(i) = b_entry(i+seeds.size(), order, std::pair{wrapper.height, wrapper.width}, seeds, 0, wrapper);
+    for (unsigned i = 0; i < b.size(); ++i)
+    {
+        b.coeffRef(i) = b_entry(i+seeds.size(), order, std::pair{wrapper.m_height, wrapper.m_width}, seeds, 0, wrapper);
     }
     auto L_u = get_L_u(order, seeds, wrapper);
     solver.compute(L_u);
@@ -28,58 +30,65 @@ void main_alg(Eigen::VectorXd& X, const std::vector<std::pair<unsigned, unsigned
 }
 
 void form_final_img(PNG& image, const PNG& mask, const Eigen::VectorXd& X, unsigned ss,
-                    const std::vector<std::pair<unsigned, unsigned>>& order) {
+                    const std::vector<std::pair<unsigned, unsigned>>& order)
+{
     /// set seeded
-    for (unsigned y = 0; y < image.height; ++y) {
-        for (unsigned x = 0; x < image.width; ++x) {
-            if (mask.R.coeffRef(x,y) == 0) {    /// definitely background
-                image.R.coeffRef(x,y) = 0;
-                image.G.coeffRef(x,y) = 0;
-                image.B.coeffRef(x,y) = 0;
+    for (unsigned y = 0; y < image.m_height; ++y)
+    {
+        for (unsigned x = 0; x < image.m_width; ++x)
+        {
+            if (mask.m_R.coeffRef(x, y) == 0)   /// definitely background
+            {
+                image.m_R.coeffRef(x, y) = 0;
+                image.m_G.coeffRef(x, y) = 0;
+                image.m_B.coeffRef(x, y) = 0;
             }
         }
     }
     /// set unseeded
-    for (unsigned x_idx = 0; x_idx < X.size(); ++x_idx) {
-        if (X.coeffRef(x_idx) > 0.5) {
-            image.R.coeffRef(order[x_idx+ss].first, order[x_idx+ss].second) = 0;
-            image.G.coeffRef(order[x_idx+ss].first, order[x_idx+ss].second) = 0;
-            image.B.coeffRef(order[x_idx+ss].first, order[x_idx+ss].second) = 0;
+    for (unsigned x_idx = 0; x_idx < X.size(); ++x_idx)
+    {
+        if (X.coeffRef(x_idx) > 0.5)
+        {
+            image.m_R.coeffRef(order[x_idx + ss].first, order[x_idx + ss].second) = 0;
+            image.m_G.coeffRef(order[x_idx + ss].first, order[x_idx + ss].second) = 0;
+            image.m_B.coeffRef(order[x_idx + ss].first, order[x_idx + ss].second) = 0;
         }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     if(argc != 4) {std::cerr << "invalid number of arguments. Must be 3" << std::endl; abort();}
 
     PNG image{argv[1]};
-    image.read_png_file();
+    image.readPngFile();
     PNG mask{argv[2]};
-    mask.read_png_file();
+    mask.readPngFile();
 
     /*unsigned seed_types = 2;*/
     std::vector<std::pair<unsigned, unsigned>> order;
     std::map<std::pair<unsigned, unsigned>, unsigned> seeds;
 
     /// initialize seeds
-    for (unsigned y = 0; y < image.height; ++y)
+    for (unsigned y = 0; y < image.m_height; ++y)
     {
-        for (unsigned x = 0; x < image.width; ++x)
+        for (unsigned x = 0; x < image.m_width; ++x)
         {
-            if (image.R.coeffRef(x,y) == 1) /// white
+            if (image.m_R.coeffRef(x, y) == 1) /// white
             {
                 for (const auto& neighbour: adjacent_nodes(std::pair<unsigned, unsigned>{x,y},
-                                                           std::pair{image.height, image.width}))
+                                                           std::pair{image.m_height, image.m_width}))
                 {
-                    if (image.R.coeffRef(neighbour.first,neighbour.second) != 0 && image.R.coeffRef(neighbour.first,neighbour.second) != 1)
+                    if (image.m_R.coeffRef(neighbour.first, neighbour.second) != 0 && image.m_R.coeffRef(neighbour.first, neighbour.second) != 1)
                         seeds[std::pair<unsigned, unsigned>{x,y}] = 0;  /// 0 means it's the thing
                 }
-            } else if (image.R.coeffRef(x,y) == 0)  /// black
+            } else if (image.m_R.coeffRef(x, y) == 0)  /// black
             {
                 for (const auto& neighbour: adjacent_nodes(std::pair<unsigned, unsigned>{x,y},
-                                                           std::pair{image.height, image.width}))
+                                                           std::pair{image.m_height, image.m_width}))
                 {
-                    if (image.R.coeffRef(neighbour.first,neighbour.second) != 0 && image.R.coeffRef(neighbour.first,neighbour.second) != 1)
+                    if (image.m_R.coeffRef(neighbour.first, neighbour.second) != 0 && image.m_R.coeffRef(neighbour.first, neighbour.second) != 1)
                         seeds[std::pair<unsigned, unsigned>{x,y}] = 1;  /// 1 means it's the background
                 }
             }
@@ -90,8 +99,10 @@ int main(int argc, char *argv[]) {
     for (const auto& el: seeds)
         order.emplace_back(el.first);  /// seeded pixels must be first
 
-    for (unsigned i = 0; i < image.height; ++i) {
-        for (unsigned j = 0; j < image.width; ++j) {
+    for (unsigned i = 0; i < image.m_height; ++i)
+    {
+        for (unsigned j = 0; j < image.m_width; ++j)
+        {
             auto temp = std::pair{i,j};
             if (seeds.find(temp) == seeds.end())
                 order.emplace_back(temp);       /// now add unseeded
@@ -102,6 +113,6 @@ int main(int argc, char *argv[]) {
     auto X = Eigen::VectorXd{};
     main_alg(X, std::ref(order), std::ref(seeds), std::ref(image));
     form_final_img(image, mask, X, seeds.size(), order);
-    image.write_out(argv[3]);
+    image.writeOut(argv[3]);
     return 0;
 }
--- src/rw_image.cpp
+++ src/rw_image.cpp
@@ -2,70 +2,71 @@
 
 #include "../inc/rw_image.h"
 
-PNG::PNG(const char *filename) : int_range(false)
+PNG::PNG(const char *filename) : m_int_range(false)
 {
-    fp = fopen(filename, "rb");
+    m_fp = fopen(filename, "rb");
 
-    png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-    if(!png) abort();
-    info = png_create_info_struct(png);
-    if(!info) abort();
-    png_init_io(png, fp);
+    m_png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    if(!m_png) abort();
+    m_info = png_create_info_struct(m_png);
+    if(!m_info) abort();
+    png_init_io(m_png, m_fp);
 
-    png_read_info(png, info);
+    png_read_info(m_png, m_info);
 
 
-    width      = png_get_image_width(png, info);
-    height     = png_get_image_height(png, info);
-    color_type = png_get_color_type(png, info);
-    bit_depth  = png_get_bit_depth(png, info);
+    m_width      = png_get_image_width(m_png, m_info);
+    m_height     = png_get_image_height(m_png, m_info);
+    m_colorType = png_get_color_type(m_png, m_info);
+    m_bitDepth  = png_get_bit_depth(m_png, m_info);
 
-    row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-    for(unsigned y = 0; y < height; y++) {
-        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
+    m_rowPointers = (png_bytep*)malloc(sizeof(png_bytep) * m_height);
+    for(unsigned y = 0; y < m_height; y++)
+    {
+        m_rowPointers[y] = (png_byte*)malloc(png_get_rowbytes(m_png, m_info));
     }
-    R = Eigen::MatrixXd{height, width};
-    G = Eigen::MatrixXd{height, width};
-    B = Eigen::MatrixXd{height, width};
+    m_R = Eigen::MatrixXd{m_height, m_width};
+    m_G = Eigen::MatrixXd{m_height, m_width};
+    m_B = Eigen::MatrixXd{m_height, m_width};
 }
 
-void PNG::read_png_file()
+void PNG::readPngFile()
 {
-    /*if(setjmp(png_jmpbuf(png))) abort();*/
-    if(bit_depth == 16)
-        png_set_strip_16(png);
+    /*if(setjmp(png_jmpbuf(m_png))) abort();*/
+    if(m_bitDepth == 16)
+        png_set_strip_16(m_png);
 
-    if(color_type == PNG_COLOR_TYPE_PALETTE)
-        png_set_palette_to_rgb(png);
+    if(m_colorType == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(m_png);
 
     // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
-    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-        png_set_expand_gray_1_2_4_to_8(png);
+    if(m_colorType == PNG_COLOR_TYPE_GRAY && m_bitDepth < 8)
+        png_set_expand_gray_1_2_4_to_8(m_png);
 
-    if(png_get_valid(png, info, PNG_INFO_tRNS))
-        png_set_tRNS_to_alpha(png);
+    if(png_get_valid(m_png, m_info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(m_png);
 
-    // These color_type don't have an alpha channel then fill it with 0xff.
-    if(color_type == PNG_COLOR_TYPE_RGB ||
-       color_type == PNG_COLOR_TYPE_GRAY ||
-       color_type == PNG_COLOR_TYPE_PALETTE)
-        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+    // These m_colorType don't have an alpha channel then fill it with 0xff.
+    if(m_colorType == PNG_COLOR_TYPE_RGB ||
+       m_colorType == PNG_COLOR_TYPE_GRAY ||
+       m_colorType == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(m_png, 0xFF, PNG_FILLER_AFTER);
 
-    if(color_type == PNG_COLOR_TYPE_GRAY ||
-       color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-        png_set_gray_to_rgb(png);
+    if(m_colorType == PNG_COLOR_TYPE_GRAY ||
+       m_colorType == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(m_png);
 
-    png_read_update_info(png, info);
+    png_read_update_info(m_png, m_info);
 
-    png_read_image(png, row_pointers);
-    check_range();
-    to_matrix();
+    png_read_image(m_png, m_rowPointers);
+    checkRange();
+    toMatrix();
 
-    png_destroy_read_struct(&png, &info, nullptr);
-    fclose(fp);     /// could close in destructor tho
+    png_destroy_read_struct(&m_png, &m_info, nullptr);
+    fclose(m_fp);     /// could close in destructor tho
 }
 
-void PNG::write_out(char *filename) const
+void PNG::writeOut(char *filename) const
 {
     FILE *fpw = fopen(filename, "wb");
     if(!fpw) abort();
@@ -76,7 +77,7 @@ void PNG::write_out(char *filename) const
     png_infop info1 = png_create_info_struct(png1);
     if (!info1) abort();
 
-    /*if (setjmp(png_jmpbuf(png))) abort();*/
+    /*if (setjmp(png_jmpbuf(m_png))) abort();*/
 
     png_init_io(png1, fpw);
 
@@ -84,7 +85,7 @@ void PNG::write_out(char *filename) const
     png_set_IHDR(
             png1,
             info1,
-            width, height,
+            m_width, m_height,
             8,
             PNG_COLOR_TYPE_RGBA,
             PNG_INTERLACE_NONE,
@@ -95,63 +96,74 @@ void PNG::write_out(char *filename) const
 
     // To remove the alpha channel for PNG_COLOR_TYPE_RGB format,
     // Use png_set_filler().
-    //png_set_filler(png, 0, PNG_FILLER_AFTER);
+    //png_set_filler(m_png, 0, PNG_FILLER_AFTER);
 
-    if (!row_pointers) abort();
+    if (!m_rowPointers) abort();
 
-    from_matrix();
-    png_write_image(png1, row_pointers);
+    fromMatrix();
+    png_write_image(png1, m_rowPointers);
     png_write_end(png1, nullptr);
 
-    for(unsigned k = 0; k < height; k++) {
-        free(row_pointers[k]);
+    for(unsigned k = 0; k < m_height; k++)
+    {
+        free(m_rowPointers[k]);
     }
-    free(row_pointers);
+    free(m_rowPointers);
 
     fclose(fpw);
 
     png_destroy_write_struct(&png1, &info1);
 }
 
-void PNG::to_matrix()
+void PNG::toMatrix()
 {
-    for(unsigned y = 0; y < height; y++) {
-        png_bytep row = row_pointers[y];
-        for(unsigned x = 0; x < width; x++) {
+    for(unsigned y = 0; y < m_height; y++)
+    {
+        png_bytep row = m_rowPointers[y];
+        for(unsigned x = 0; x < m_width; x++)
+        {
             png_bytep px = &(row[x * 4]);
-            if (int_range) {
-                R.coeffRef(y,x) = *px/255.0;
-                G.coeffRef(y,x) = *(px+1)/255.0;
-                B.coeffRef(y,x) = *(px+2)/255.0;
-            } else {
-                R.coeffRef(y,x) = *px;
-                G.coeffRef(y,x) = *(px+1);
-                B.coeffRef(y,x) = *(px+2);
+            if (m_int_range)
+            {
+                m_R.coeffRef(y, x) = *px / 255.0;
+                m_G.coeffRef(y, x) = *(px + 1) / 255.0;
+                m_B.coeffRef(y, x) = *(px + 2) / 255.0;
+            } else
+            {
+                m_R.coeffRef(y, x) = *px;
+                m_G.coeffRef(y, x) = *(px + 1);
+                m_B.coeffRef(y, x) = *(px + 2);
             }
         }
     }
 }
 
-void PNG::from_matrix() const
+void PNG::fromMatrix() const
 {
-    for(unsigned y = 0; y < height; y++) {
-        png_bytep row = row_pointers[y];
-        for(unsigned x = 0; x < width; x++) {
+    for(unsigned y = 0; y < m_height; y++)
+    {
+        png_bytep row = m_rowPointers[y];
+        for(unsigned x = 0; x < m_width; x++)
+        {
             png_bytep px = &(row[x * 4]);
-            *px = round(R.coeffRef(y, x) * 255);
-            *(px+1) = round(G.coeffRef(y, x) * 255);
-            *(px+2) = round(B.coeffRef(y, x) * 255);
+            *px = round(m_R.coeffRef(y, x) * 255);
+            *(px+1) = round(m_G.coeffRef(y, x) * 255);
+            *(px+2) = round(m_B.coeffRef(y, x) * 255);
         }
     }
 }
 
-void PNG::check_range() {
-    for(unsigned y = 0; y < height; y++) {
-        png_bytep row = row_pointers[y];
-        for(unsigned x = 0; x < width; x++) {
+void PNG::checkRange()
+{
+    for(unsigned y = 0; y < m_height; y++)
+    {
+        png_bytep row = m_rowPointers[y];
+        for(unsigned x = 0; x < m_width; x++)
+        {
             png_bytep px = &(row[x * 4]);
-            if (*px > 1 || *(px+1) > 1 || *(px+2) > 1) {
-                int_range = true;
+            if (*px > 1 || *(px+1) > 1 || *(px+2) > 1)
+            {
+                m_int_range = true;
                 return;
             }
         }
